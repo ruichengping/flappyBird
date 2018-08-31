93f9914401ab158f7c0764a2a170a99b83c6017a
长卿 <changqing@dtstack.com>
2018-09-01 00:45:34 +0800

    加入管子移动

--- js/main.js
+++ js/main.js
@@ -1,32 +1,34 @@
 (function() {
     var canvas=document.getElementById('flappy_bird');
-    setCanvasView(canvas);
-    window.onresize=function(){
-        setCanvasView(canvas);
-    }
     var context=canvas.getContext('2d');
     var loading=document.getElementById('loading');
+    var pipeList=[];
+    var resourceModel={};
+    var gameprocess;
+    var pipeDistance=180;
+    
+    window.onresize=function(){
+        setCanvasView(canvas);
+        clearInterval(gameprocess);
+        creatMainProcess(resourceModel);
+    }
 
     loadResource({
         'background_day':'images/bg_day.png',
         'land':'images/land.png',
+        'pipeDown':'images/pipe_down.png',
+        'pipeUp':'images/pipe_up.png',
         'birdOne':'images/bird0_0.png',
         'birdTwo':'images/bird0_1.png',
         'birdThree':'images/bird0_2.png'
-    },function (resourceModel) {
-        var x=0;
-        var gameprocess=setInterval(function () {
-            clearCanvas();
-            x=x-2<-canvas.width?0:x-2;
-            drawBackground(resourceModel.background_day);
-            drawLand(resourceModel.land,x);
-        },30);
+    },function () {
+       setCanvasView(canvas);
+       creatMainProcess(resourceModel);
     });
 
     //加载资源
     function loadResource(resources,callback) {
         var count=0;
-        var resourceModel={};
         _.keys(resources).forEach(function (key) {
             resourceModel[key]=new Image();
             resourceModel[key].src=resources[key];
@@ -42,29 +44,102 @@
         });
     }
 
+    //初始化
+    function creatMainProcess(resourceModel){
+        var landX=0;
+        var pipeX=canvas.width*0.5;  
+        var land=new Land(resourceModel.land);
+        var background=new Background(resourceModel.background_day);
+        var pipe=new Pipe(resourceModel.pipeDown,resourceModel.pipeUp,resourceModel.land);
+        pipeList=[pipe];
+        gameprocess=setInterval(function () {
+            clearCanvas();
+            landX=landX-3<-canvas.width?0:landX-3;
+            pipeX-=3;
+            //画北京
+            background.render();
+            //画大地
+            land.render(landX);
+            if(Math.floor(pipeX/pipeDistance*-1)+1>pipeList.length){
+                pipeList.push(new Pipe(resourceModel.pipeDown,resourceModel.pipeUp,resourceModel.land));
+            }
+            //画管子
+            pipeList.forEach(function(pipe,index){
+                pipe.render(pipeX+pipeDistance*index);
+            });
+        },30);
+    }
+
     //清空画布
     function clearCanvas() {
         context.clearRect(0,0,canvas.width,canvas.height);
     }
-    //画背景
-    function drawBackground(background) {
+    /**
+     * 背景
+     * @param {Object<Image>} image 背景图片
+     */
+    function Background(image){
+        this.background=image;
+    }
+    Background.prototype.render=function(){
+        var background=this.background;
         context.drawImage(background,0,0,canvas.width,canvas.height);
     }
-    //画大地
-    function drawLand(land,x) {
+  
+    /**
+     * 大地
+     * @param {Object<Image>} image 大地图片
+     */
+    function Land(image){
+        this.land=image;
+    }
+    
+    Land.prototype.render=function(x){
+        var land=this.land;
         context.drawImage(land,x,canvas.height-land.height,canvas.width,land.height);
         context.drawImage(land,canvas.width+x,canvas.height-land.height,canvas.width,land.height);
     }
     
+    /**
+     * 
+     * @param {Object<Image>} pipeDownImage 上管子图片
+     * @param {Object<Image>} pipeUpImage 下管子图片
+     * @param {Object<Image>} landImage 大地
+     */
+    function Pipe(pipeDownImage,pipeUpImage,landImage) {
+        //每对管子的唯一标识
+        this.id=new Date().getTime();
+        //随机数
+        this.random=Math.round(Math.random()*80);
+        //大地图片对象
+        this.land=landImage;
+        //向下管子图片对象
+        this.pipeDown=pipeDownImage;
+        //向上管子图片对象
+        this.pipeUp=pipeUpImage;
+    }
+    Pipe.prototype.render=function (x) {
+        var land=this.land;
+        var distance=160;
+        var pipeAverageHeight=(canvas.height-land.height-distance)*0.5
+        var pipeDown=this.pipeDown;
+        var pipeUp=this.pipeUp;
+        var pipeUpHeight=pipeAverageHeight*(40+this.random)/100;
+        var pipeDownHeight=canvas.height-land.height-distance-pipeUpHeight;
+        if(x+pipeDown.width+canvas.width>0){
+            //画向下管子
+            context.drawImage(pipeDown,0,pipeDown.height-pipeDownHeight,pipeDown.width,pipeDownHeight,x+canvas.width,0,pipeDown.width,pipeDownHeight); 
+            //画向上管子
+            context.drawImage(pipeUp,0,0,pipeUp.width,pipeUpHeight,x+canvas.width,canvas.height-land.height-pipeUpHeight,pipeUp.width,pipeUpHeight);            
+        }
+    }
 
-    
-    //管子
-    function Pipe() {
+    function Bird(){
 
     }
-    
-    Pipe.prototype.render=function () {
-        
+
+    Bird.prototype.render=function(){
+
     }
 })();
 
