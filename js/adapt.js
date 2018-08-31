dba4db6f8e87a435f83251f599ffbca73cc34afd
长卿 <changqing@dtstack.com>
2018-08-31 15:47:38 +0800

    页面布局

--- js/adapt.js
+++ js/adapt.js
@@ -1,15 +1,8 @@
 function calculateCanvas(){
   var width=375;
   var height=667;
-  var view_width,view_height;
-
-  if(window.innerWidth){
-    view_width=window.innerWidth;
-    view_height=window.innerHeight;
-  }else{
-    view_width=document.documentElement.clientWidth;
-    view_height=document.documentElement.clientHeight;
-  }
+  var view_width=document.body.clientWidth;
+  var view_height=document.body.clientHeight;
 
   if(view_width<992){
     width=view_width;
